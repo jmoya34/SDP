226d6fc1db64dd7a6442d4852c50225ebcbd0201
jmoya34 <jasonmoya34@gmail.com>
2024-03-19 20:54:13 -0700

    slight changes to uart input

--- PI_scripts/uart_test.py
+++ PI_scripts/uart_test.py
@@ -4,8 +4,8 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) # Open serial port at 9600 baud
 ser.reset_input_buffer()
 while True:
-    motor1_speed = input("motor1_speed")
-    motor2_speed = input("motor2_speed")
+    motor1_speed = input("motor1_speed: ")
+    motor2_speed = input("motor2_speed: ")
     command = "{},{}\n".format(motor1_speed, motor2_speed)
     ser.write(command.encode('utf-8'))
     line = ser.readline().decode('utf-8').rstrip()
